1ccb409a93c6527b12361db128fe709f6dd081c5
Dmitry-Nazaruk <12nazaruk@gmail.com>
2022-05-26 20:55:09 +0300

    Changed the check for the number of characters, now an error is raised when the number of elements is not equal to three

--- w1.py
+++ w1.py
@@ -1,11 +1,11 @@
 class FormulaError(Exception):
-    def __init__(self,message='FormulaError'):
+    def __init__(self, message='FormulaError'):
         self.message = message
         super().__init__(self.message)
 
 def calc(input_data:str):
     split_data = input_data.split()
-    if len(split_data) < 3:
+    if len(split_data) != 3:
         raise FormulaError
     try:
         float_number_first, float_number_second = float(split_data[0]),float(split_data[2])
