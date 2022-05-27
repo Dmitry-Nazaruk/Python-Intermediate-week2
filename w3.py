e9eedcc59d71ae2bb5616761824d2460c834c0a1
Dmitry-Nazaruk <12nazaruk@gmail.com>
2022-05-27 20:26:27 +0300

    Add function fib_list and change fib_generator to work with fib_list

--- w3.py
+++ w3.py
@@ -1,8 +1,13 @@
-def fib_generator(n:int):
-    lst = [0,1]
+def fib_generator(n: list):
+    for i in n:
+        yield i
+
+
+def fib_list(n: int) -> list:
+    lst = [0, 1]
     while len(lst) <= n:
         lst.append(lst[-1] + lst[-2])
-    yield lst[:n]
+    return lst[:n]
 
-for i in fib_generator(10):
+for i in fib_generator(fib_list(3)):
     print(i)
